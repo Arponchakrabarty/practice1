1705ff5eb7f9c7c6d52b9207b487d8913cf348ad
Arponchakrabarty <arponchakrabarty71@gmail.com>
2026-08-26 15:17:41 +0600

    new

--- lvl5/3.js
+++ lvl5/3.js
@@ -1,1 +1,15 @@
 // Find the GCD of two numbers.
+ let a= 15
+ let b= 25
+ let extra
+
+ for(let i=1; i<2 ; i++){
+    let A= b%a 
+    extra= A
+    if (a%extra===0){
+        console.log(extra);
+    }
+    else{
+        console.log(A);
+    }
+ }
